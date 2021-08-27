162a9a0e51c138efcb5428a984a09ba0c09a4fcf
usuario <evb1039@gmail.com>
2021-08-26 20:41:05 -0500

    tarea hecha hasta deObjetoAmatriz

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,11 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var array = [];
+  for(let clave in objeto){
+    array.push([clave,objeto[clave]]);
+  }
+  return array;
 }
 
 
@@ -18,6 +23,11 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var obj = {};
+  for(let i = 0; i < string.length;i++){
+    obj[string[i]] += 1;
+  }
+  return obj;
 }
 
 
